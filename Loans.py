459cdb47c2ba2d68a2d1d48898b277bf87fae1b3
Олег <solegva@ya.ru>
2024-11-01 20:15:07 +0300

    01.11.24 Loans

--- Loans.py
+++ Loans.py
@@ -10,9 +10,34 @@
 # постоянно высчитывает суммы, которые необходимо отдать и сумму собственных средств
 # вычисляет среднюю сумму получаемой прибыли в день и неделю
 # вносит календарное деление день недели, дата
+# Комментарий необходимо переработать в соответствии с составленой задачей
 
-Balance = [] # общая сумма средств
+# Replenishment of the Balance
+
+Replenishment_of_the_Balance = [179.57, 51.3, 112.87, 205.54, 225.55, 3000, 1003.52, 1000.5]
+R = sum(Replenishment_of_the_Balance)
+print(f'Replenishment of the Balance: {R} USDT')
+
+# Profit
+
+Profit = [451.03, 34.94, 163.4, 175.96, 209.93, 167.99, 100.04, 101.64, 133.34, 2037.15]
+P = sum(Profit)
+print(f'Profit: {P} USDT')
+
+# Loans
+
 Broker_Loan = [3000]
-print(f'{Broker_Loan}')
-People_loan = []
+People_loan = [1000]
+Loans = Broker_Loan + People_loan
+Loan_Repayment = [1000, 1000]
+LR = sum(Loan_Repayment)
+Existing_Loan = Loans - LR
+
+# We need to know what sum of loans I still need to pay
+
+print(f'{Existing_Loan}')
+
+# Balance
+Balance = (R + P) - LR
+print(f'Balance: {Balance} USDT')
 
