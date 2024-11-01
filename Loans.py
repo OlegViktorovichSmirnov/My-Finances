6ca9aab0ff5157633a90028d289908d2ade59087
Олег <solegva@ya.ru>
2024-11-01 21:01:06 +0300

    01.11.24 Loans

--- Loans.py
+++ Loans.py
@@ -20,7 +20,7 @@ print(f'Replenishment of the Balance: {R} USDT')
 
 # Profit
 
-Profit = [451.03, 34.94, 163.4, 175.96, 209.93, 167.99, 100.04, 101.64, 133.34, 2037.15]
+Profit = [442.42, 34.94, 163.4, 175.96, 209.93, 167.99, 100.04, 101.64, 133.34, 143.76, 2037.15]
 P = sum(Profit)
 print(f'Profit: {P} USDT')
 
@@ -28,14 +28,14 @@ print(f'Profit: {P} USDT')
 
 Broker_Loan = [3000]
 People_loan = [1000]
-Loans = Broker_Loan + People_loan
+# Loans = Broker_Loan + People_loan
 Loan_Repayment = [1000, 1000]
 LR = sum(Loan_Repayment)
-Existing_Loan = Loans - LR
+# Existing_Loan = Loans - LR
 
 # We need to know what sum of loans I still need to pay
 
-print(f'{Existing_Loan}')
+# print(f'{Existing_Loan}')
 
 # Balance
 Balance = (R + P) - LR
