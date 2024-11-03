aac1a516f5dccb093438ac3cdb21991cdeb650d0
Олег <solegva@ya.ru>
2024-11-03 19:55:18 +0300

    03.11.24 Cost of living

---
+++ Cost of Living.py
@@ -0,0 +1,24 @@
+# Buenos Aires
+#
+# Rent & Utilities
+
+# 1 bedroom apartment in city Center, 40m2 or 430ft2, average
+Rent = 450
+
+# electricity, heating, water, etc.
+Utility_Bill = 34.8
+
+# 50Mbps+ 1 month unlimited
+Internet_plan = 22.6
+
+def get_Food(milk, bread, cheese, eggs, steaks, apples, oranges, tomatoes, potatoes, onions, water, wine, beer):
+    Food = []
+
+# Cost of food 139.83
+cost_of_food = get_Food(13.1, 42.6, 10.4, 2.28, 14.84, 11.58, 7.92, 8.76, 5.64, 2.24, 3.36, 4.65, 12.5)
+f1 = sum(cost_of_food)
+print(f'{f1}')
+
+# Consumables
+Shower_gel = 5.11
+Toilet_paper = 7.72
